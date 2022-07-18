b78487f46198dbd9fd4be6ff442b05d83a8ffd2e
Marvin <marvinwanzuita@gmail.com>
2022-07-18 13:54:51 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 
 public class Program {
@@ -15,7 +12,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 			
 	}
 
